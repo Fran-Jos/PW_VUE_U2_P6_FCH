66253979f0e7d5a1892d2308e507e7e3da940020
FranJos <fjchanataxi@uce.edu.ec>
2024-06-12 19:52:30 -0500

    taller19 consumo de api

--- src/components/Pregunta.vue
+++ src/components/Pregunta.vue
@@ -1,23 +1,110 @@
 <template>
-  <img
-    src="https://yesno.wtf/assets/no/16-b66d2db7543f5259c86abc166d6901cf.gif"
-    alt="No hay imagen"
-  />
-<div class="obscuro"></div>
+  <img v-if="img" :src="img" alt="No hay imagen" />
+  <div class="obscuro"></div>
   <div class="pregunta-container">
-    <input type="text" placeholder="Hazme una pregunta" />
+    <input v-model="pregunta" type="text" placeholder="Hazme una pregunta" />
     <p>Recuerda terminar la pregunta con un signo de pregunta (?)</p>
 
-    <div>
-      <h2>Voy ha pasar de semestre ?</h2>
-      <h1>SI , NO ;..........</h1>
+    <div class="respuesta">
+      <h2>{{ pregunta }} ?</h2>
+      <h1>{{ respuesta }}</h1>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pregunta: null,
+      respuesta: null,
+      img: null,
+    };
+  },
+
+  /* watch siempre va estar observando que hay un cambio 
+en la propiedad reactiva */
+  watch: {
+    /*
+  todo observador se tiene que crear con el mismo nombre de la propiedad reactiva
+  */
+    pregunta(value, oldvalue) {
+      console.log({ value, oldvalue });
+
+      if (!value.includes("?")) return;
+      this.obtenerRespuesta();
+    },
+  },
+
+  methods: {
+    async obtenerRespuesta() {
+      this.respuesta = "Pensando.......";
+      const data = await fetch("https://yesno.wtf/api").then((res) =>
+        res.json()
+      );
+      const { answer, image, forced } = data;
+      console.log(answer);
+      this.respuesta = answer;
+      this.img = image;
+      return data;
+    },
+
+    async prueba() {
+      const data2 = await this.obtenerRespuesta();
+    },
+  },
+};
 </script>
 
 <style>
+img,
+.obscuro {
+  max-height: 100%;
+  height: 100vh;
+  max-width: 100%;
+  width: 100vw;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+}
+
+.obscuro {
+  background-color: rgba(0, 0, 0, 0.7);
+  /*
+  0.0 --> full transparencia
+  0.5 --> semi transparente
+  1.0 --> full opaco
+  */
+}
+
+.pregunta-container {
+  position: relative;
+}
+
+input {
+  width: 560px;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  margin-top: 200px;
+  text-align: center;
+}
+
+input:focus {
+  outline: none;
+}
+
+p,
+h1,
+h2 {
+  color: white;
+}
+
+p {
+  font-size: 26px;
+  margin-top: 0px;
+}
+.respuesta {
+  margin-top: 100px;
+}
 </style>
